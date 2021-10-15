e84abc6ee5986ab18b12bd1ceb04ef4ab16cd339
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-10-14 22:10:48 -0400

    paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -19,10 +19,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = user.find(params[:id])
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
