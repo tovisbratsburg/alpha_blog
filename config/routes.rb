b3086f3bea062d5c810692e2e19ac2c2ed7e364a
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-11-13 15:49:11 -0500

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
   get 'about', to: 'pages#about'
+
   resources :articles
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :categories, except: [:destory]
+
+  resources :categories, except: [:destroy]
 end
